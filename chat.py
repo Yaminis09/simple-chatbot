92909149c5e85a65196b34d1fc6c5eee537b9a87
Yaminisingh09 <yamini@digbihealth.com>
2025-12-17 19:41:14 +0530

    cleaned code

--- chat.py
+++ chat.py
@@ -1,17 +1,19 @@
 from ctransformers import AutoModelForCausalLM
 
+# load model
 llm = AutoModelForCausalLM.from_pretrained("zoltanctoth/orca_mini_3B-GGUF", model_file="orca-mini-3b.q4_0.gguf")
-def get_llm():
-    
+# call model
+def get_llm():    
     return llm
 
-# promptss = "what is the capital of India? give me one word answer"
-
-# print(llm(promptss))
 history = []
 def get_prompt(instruction, history):
-    # welcome_message = "Hello! how may I help you?"
-    # prompt = f"Your question is :{instruction}\nAnswer:" 
+    """
+    Docstring for get_prompt
+    
+    :param instruction: Create prompt specific to give small and consise answers.
+    :param history: List -> Stores the user chat history
+    """   
     system = "You are an AI assistant that gives helpful answers. You answer the question in a short and concise way."
     # create prompt first
     prompt = f"### System:\n{system}\n\n"
@@ -19,46 +21,37 @@ def get_prompt(instruction, history):
     if history:
         prompt += f"{''.join(history)}"
     prompt = f"### User:\n{instruction}\n\n### Answer:\n"
-    print(prompt)
+    # print(prompt)
     return prompt
 
+llm.reset()
+def ask_question(instruction):
+    """
+    Docstring for ask_question
+    
+    :param instruction: calls LLM to yield word by word response and stream response to the UI
+    Append instruction(question) and response to list history
+    """
+    global history
+    prompt = get_prompt(instruction, history)
+    llm = get_llm()
+    llm.reset()
+    answer = ""
+    for word in llm(prompt, stream=True):    
+        # print(word, end="", flush=True)
+        answer += word
+        # stream to UI
+        yield word
+    # Append instruction to list history
+    history.append(f"User: {instruction}") 
+    # Append llm response to    
+    history.append(f"Assistant: {answer}") 
+    
 
-# instruction = input("What is your question? ")
-# prompt = get_prompt(instruction)
-
-# llm.reset()
-# for word in llm(prompt, stream=True):    
-#     print(word, end="", flush=True)
-# print()
-
-# def ask_question():
-#     prompt = get_prompt(instruction, history)
-#     llm = get_llm()
-#     llm.reset()
-#     answer = ""
-#     for word in llm(prompt, stream=True):    
-#         print(word, end="", flush=True)
-#         answer += word
 
 
-#     pass
 
-# while True:
-#     instruction = input("What is your question? (To end the chat type 'bye') ")
-#     # list of words to end the while loop
-#     if instruction.lower() in ["end","bye","That's it", "Good bye", "exit"]:
-#         break
 
-#     prompt = get_prompt(instruction, history)
-#     llm = get_llm()
-#     llm.reset()
-#     answer = ""
-#     for word in llm(prompt, stream=True):    
-#         print(word, end="", flush=True)
-#         answer += word
-#     print()
-#     history.append(f"User: {instruction}")    
-#     history.append(f"Assistant: {answer}") 
 
     
 
